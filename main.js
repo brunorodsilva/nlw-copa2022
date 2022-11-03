b3b35fd985f8989488eb667ff740d3d0f20a9913
brunorodsilva <brunord2014@hotmail.com>
2022-11-03 14:24:54 -0300

    Inserção de todas as bandeiras

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function createGame(player1, hour, player2){
     return `
         <li>
-            <img src="./assets/icon-${player1}.svg"
+            <img class="calender-flags" src="./assets/${player1}.svg"
             alt="Bandeira do ${player1}">
             <strong>${hour}</strong>
-            <img src="./assets/icon-${player2}.svg"
+            <img class="calender-flags" src="./assets/${player2}.svg"
             alt="Bandeira da ${player2}">
         </li>
     `
@@ -23,11 +23,33 @@ function createCard(date, day, games){
     `
 }
 document.querySelector('#cards').innerHTML =
+        createCard("20/11", "domingo", 
+        createGame("qatar", "16:00", "ghana")) +
+        createCard("21/11", "segunda",
+            createGame("england", "10:00", "iran")+
+            createGame("senegal", "13:00","serbia")+
+            createGame("unitedstates", "16:00","wales")) +         
+        createCard("22/11", "terça", createGame("brazil", "16:00",
+        "serbia")) +
+        createCard("23/11", "quarta", createGame("brazil", "16:00",
+        "serbia")) +
         createCard("24/11", "quinta", createGame("brazil", "16:00",
         "serbia")) +
-        createCard("28/11", "segunda",
+        createCard("25/11", "sexta", createGame("brazil", "16:00",
+        "serbia")) +
+        createCard("26/11", "sábado", createGame("brazil", "16:00",
+        "serbia")) +
+        createCard("27/11", "domingo", createGame("brazil", "16:00",
+        "serbia")) +
+        createCard("28/11", "segunda", createGame("brazil", "16:00",
+        "serbia")) +
+        createCard("29/11", "terça", createGame("brazil", "16:00",
+        "serbia")) +
+        createCard("30/11", "quarta",
             createGame("brazil", "13:00", "switzerland") + 
             createGame("portugal", "16:00", "uruguai")
         ) +
+        createCard("01/12", "quinta", 
+        createGame("brazil", "16:00", "ghana"))+
         createCard("02/12", "sexta", 
         createGame("brazil", "16:00", "ghana"))
